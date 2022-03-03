538ec53ae0d3e22aefbe37931bc8d8879e362c29
admire <admire@kartoza.com>
2022-03-03 10:18:36 +0200

    Get repo in sync with latest changes from staging env

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # SAGTA
-SAGTA Map downloader and other
+SAGTA Map downloader
+
+## Setup
+
+This repo is based on the work provided from [lizmap](git@github.com:3liz/lizmap-docker-compose.git)
+To get a running version of the repo use the following instructions
+
+* Clone the sagta repository into a separate folder.
+
+    ```bash
+    git clone git@github.com:kartoza/SAGTA.git
+    ```
+* Clone the lizmap github repository
+
+    ```bash
+    git clone git@github.com:3liz/lizmap-docker-compose.git
+    ```
+* Navigate to the `lizmap-docker` folder
+* Run `make configure` to generate the configs in the folder.
+* Remove the test QGIS projects from the repo.
+    ```bash
+    rm -rf lizmap/instances/*
+    ```
+* Download the latest compose file from the SAGTA repo to overwrite the 
+current one.
+
+  ```bash
+  wget https://raw.githubusercontent.com/kartoza/SAGTA/main/deployment/docker-compose.yaml
+  ```
+* Download the `env.example` from the SAGTA repo and make sure to adjust the
+env variable to match the path for the folders in the current directory
+
+  ```bash
+  wget https://raw.githubusercontent.com/kartoza/SAGTA/main/deployment/env.example -O .env
+  ```
+**Note:** Make sure you adjust the two variables to match the proper paths.
+
+  ```bash
+  LIZMAP_PROJECTS=/tmp/lizmap/docker-compose/lizmap/instances
+  LIZMAP_DIR=/tmp/lizmap/docker-compose/lizmap 
+  ```
+* Copy the `plugins` folder from the [SAGTA plugin folder](https://github.com/kartoza/SAGTA/tree/main/plugins) 
+you have cloned previously into the folder `lizmap/plugins/`
+* Copy the QGIS projects from the [SAGTA QGIS projects](https://github.com/kartoza/SAGTA/tree/main/projects/map_downloader) 
+you have previously cloned to `lizmap/instances/`
+* Get the services up by running `docker-compose up -d`
+* The default login credentials are `a`
+
+
---
+++ projects/map_downloader/qgis_scripts/sagta_grid_functions.py
@@ -0,0 +1,76 @@
+import math
+import typing
+from string import ascii_uppercase
+
+from qgis.core import *
+from qgis.gui import *
+
+@qgsfunction(args='auto', group='sagta')
+def get_grid_interval(map_scale, feature, parent):
+    current_scale = float(map_scale)
+    if map_scale >= 150000:
+        result = 0.066666666668
+    elif 99999 <= map_scale < 150000:
+        result = 0.033333333334
+    elif 49999 <= map_scale < 99999:
+        result = 0.016666666667
+    elif 24999 <= map_scale < 49999:
+        result = 0.0083333333335
+    else:
+        result = 0.00416666666675
+    return result
+        
+    
+@qgsfunction(args='auto', group='sagta')
+def get_grid_label(
+        map_width,
+        extent_min, 
+        interval_width, 
+        current_grid_value,
+        use_letters,
+        reversed,
+        feature, 
+        parent, 
+        context
+):
+    """
+    Calculates the current X label.
+    <h2>Example usage:</h2>
+    <ul>
+      <li>my_sum(5, 8) -> 13</li>
+      <li>my_sum("field1", "field2") -> 42</li>
+    </ul>
+    """
+    num_intervals = math.ceil(map_width / interval_width)
+    current_position = current_grid_value - extent_min
+    for i in range(num_intervals):
+        interval_start = extent_min + i * interval_width
+        interval_end = extent_min + (i + 1) * interval_width
+        if interval_start <= current_grid_value < interval_end:
+            result = get_choices_label(
+                i, 
+                num_intervals, 
+                reverse_order=reversed,
+                choices=ascii_uppercase if use_letters else None,
+            )
+            break
+    else:
+        result = 'error'
+    return result
+    
+    
+def get_choices_label(
+        current_item: int,
+        total_items: int,
+        reverse_order: typing.Optional[bool] = False,
+        choices: typing.Optional[typing.Iterable] = None,
+        offset: typing.Optional[int] = 1,
+) -> str:
+    if choices is None:
+        possible_outputs = range(total_items)
+    else:
+        possible_outputs = choices
+    output_values = list(possible_outputs)[:total_items]
+    if reverse_order:
+        output_values.reverse()
+    return output_values[current_item + offset]
