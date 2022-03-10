80df1ffeaa2139f5e3c3ab66e8b9ac736e71ed6c
Divan Vermeulen <79740955+vermeulendivan@users.noreply.github.com>
2022-03-10 10:53:30 +0200

    Last grid block label for the x-axis fix.
    A bug which caused the last grid block for the x-axis has been resolved.

--- plugins/custom_grid_label/customgridlabel/gridlabelfunctions.py
+++ plugins/custom_grid_label/customgridlabel/gridlabelfunctions.py
@@ -5,11 +5,11 @@ from string import ascii_uppercase
 from qgis.core import *
 from qgis.gui import *
 
-GROUP_NAME = 'Plugin - Custom grid label'
+from qgis.PyQt.QtCore import *
 
-
-@qgsfunction(args='auto', group=GROUP_NAME)
+@qgsfunction(args='auto', group='sagta')
 def get_grid_interval(map_scale, feature, parent):
+    current_scale = float(map_scale)
     if map_scale >= 150000:
         result = 0.066666666668
     elif 99999 <= map_scale < 150000:
@@ -21,9 +21,9 @@ def get_grid_interval(map_scale, feature, parent):
     else:
         result = 0.00416666666675
     return result
-
-
-@qgsfunction(args='auto', group=GROUP_NAME)
+        
+    
+@qgsfunction(args='auto', group='sagta')
 def get_grid_label(
         map_width,
         extent_min,
@@ -35,37 +35,53 @@ def get_grid_label(
         parent,
         context
 ):
-    """Calculates the current grid label"""
-    num_intervals = math.ceil(map_width / interval_width)
-    current_position = current_grid_value - extent_min
-    for i in range(num_intervals):
-        interval_start = extent_min + i * interval_width
-        interval_end = extent_min + (i + 1) * interval_width
+    """
+    Calculates the current X label.
+    <h2>Example usage:</h2>
+    <ul>
+      <li>my_sum(5, 8) -> 13</li>
+      <li>my_sum("field1", "field2") -> 42</li>
+    </ul>
+    """
+    num_intervals = math.ceil(map_width / interval_width)  # Number of grid blocks
+    current_position = current_grid_value - extent_min  # Label position
+    
+    for i in range(num_intervals):  # Loops through each of the grids
+        interval_start = extent_min + i * interval_width  # Grid start coordinates
+        interval_end = extent_min + (i + 1) * interval_width  # Grid end coordinates
+        
+        # Find the grid block which needs to be labelled
         if interval_start <= current_grid_value < interval_end:
-            result = _get_choices_label(
+            result = get_choices_label(
                 i,
                 num_intervals,
                 reverse_order=reversed,
                 choices=ascii_uppercase if use_letters else None,
             )
             break
-    else:
+    else:  # When the loop ends
         result = 'error'
+    
     return result
-
-
-def _get_choices_label(
+    
+    
+def get_choices_label(
         current_item: int,
         total_items: int,
         reverse_order: typing.Optional[bool] = False,
         choices: typing.Optional[typing.Iterable] = None,
         offset: typing.Optional[int] = 1,
 ) -> str:
-    if choices is None:
-        possible_outputs = range(total_items)
-    else:
+    output_values = None
+    if choices is None:  # x-axis, therefore integer values
+        # Increments with one to be sure to include the last grid block case
+        possible_outputs = range(total_items + 1)
+        output_values = list(possible_outputs)[:total_items + 1]
+    else:  # y-axis, therefore alphabetical characters
         possible_outputs = choices
-    output_values = list(possible_outputs)[:total_items]
-    if reverse_order:
+        output_values = list(possible_outputs)[:total_items]
+    
+    if reverse_order:  # This is only done for the y-axis - alphabetical characters
         output_values.reverse()
+    
     return output_values[current_item + offset]
